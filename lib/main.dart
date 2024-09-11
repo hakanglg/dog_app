84afc5e8e3bb6a1737506d9944255ac8f6e955b7
Hakan GÖLGE <hakangolge@marun.edu.tr>
2024-09-11 18:23:35 +0300

    auto route added

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,19 @@
+import 'package:dogapp/product/navigation/app_router.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  static  final _appRouter = AppRouter();
+
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+    return MaterialApp.router(
+      title: 'Flutter Demo',
+     routerConfig: _appRouter.config(),
     );
   }
 }
